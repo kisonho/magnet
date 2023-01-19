beba4ca1c78aaaeec5d79593f0e8e9b72c237e2a
Qisheng He <RobertHe.KsHo@wayne.edu>
2023-01-19 10:16:29 -0500

    01-19-23
    - Example code updated

--- examples/train_unetr_iseg.py
+++ examples/train_unetr_iseg.py
@@ -15,7 +15,7 @@ from torchmanager_monai import Manager, metrics
 
 import data 
 from configs import TrainingConfig
-UNETR = magnet.networks.UNETRWithDictOutput
+UNETR = magnet.networks.UNETR
 
 if __name__ == "__main__":
     # get configurations
@@ -52,8 +52,8 @@ if __name__ == "__main__":
 
     # initialize optimizer, loss, metrics, and post processing
     optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.6, weight_decay=1e-5)
-    loss_fn = losses.Loss(DiceCELoss(to_onehot_y=True, softmax=True), target="out")
-    dice_fn = metrics.CumulativeIterationMetric(DiceMetric(include_background=True, reduction="none", get_not_nans=False), target="out")
+    loss_fn = losses.Loss(DiceCELoss(to_onehot_y=True, softmax=True))
+    dice_fn = metrics.CumulativeIterationMetric(DiceMetric(include_background=True, reduction="none", get_not_nans=False))
     metric_fns: dict[str, metrics.Metric] = {"val_dice": dice_fn}
     post_labels = data.transforms.AsDiscrete(to_onehot=num_classes)
     post_predicts = data.transforms.AsDiscrete(argmax=True, to_onehot=num_classes)

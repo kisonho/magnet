b8edcde39f6ad5bdbbfd86ff10e604aa9b84f67f
Qisheng He <Qisheng.He@wayne.edu>
2024-01-12 15:53:27 -0500

    Typing improvement

--- magnet/__init__.py
+++ magnet/__init__.py
@@ -1,10 +1,9 @@
 from . import data, losses, networks, nn
 from .managers import MonaiTargetingManager as MonaiManager, TargetingManager as Manager
 from .nn import MAGNET, MAGNET2
+from .version import VERSION
 
 try:
     from .builder import build, build_v1, build_v2, build_v2_unet
 except ImportError:
     build = build_v1 = build_v2 = build_v2_unet = NotImplemented
-
-VERSION = "2.1"
